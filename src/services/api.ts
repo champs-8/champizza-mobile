bed4f59f609cfd6422096d9bbfc042e22893a4f3
champs-8 <fabio.s.dev@outlook.com>
2025-05-09 22:07:20 -0300

    Refactor code structure for improved readability and maintainability

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 //pode usar o IP para nao dar bug
 const api = axios.create({
     // baseURL: 'http://localhost:8080'
-    baseURL: 'http://172.29.64.1:8080'
+    baseURL: 'https://champizza-api.onrender.com'
 });
 
 export {api};
